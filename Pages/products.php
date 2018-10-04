8fc7b042196c69299f5008cafbcbb95b94f88e21
Quentin <quentin.consigny@isen.yncrea.fr>
2018-10-04 17:12:05 +0200

    Presentation Articles en +
    Attention : image en avant débordent sur le menue déroulant

--- Pages/products.php
+++ Pages/products.php
@@ -1,1 +1,23 @@
-welcome in products -> work in progress
+<!doctype html>
+<head><link rel="stylesheet" href="..\Styles\3columns.css"></head>
+<html>
+
+<?php include 'header.php'; ?>
+
+<body>
+	<br/><br/><br/><br/></br></br>
+	
+	<div id="col">
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+	
+		
+	</div>
+
+	
+</body>
+</html>
