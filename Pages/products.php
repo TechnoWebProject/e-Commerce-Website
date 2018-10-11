c0e8664bde1728ef800b97897fbc107695fc9920
charlesGdrt <charles.goudaert@isen.yncrea.fr>
2018-10-11 16:29:10 +0200

    demenagement

---
+++ Pages/products.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<head><link rel="stylesheet" href="..\Styles\3columns.css">
+
+<link rel="stylesheet" href="..\Styles\main.css"></head>
+
+<link rel="stylesheet" href="..\Styles\main.css">
+
+<link rel="stylesheet" href="..\Styles\products.css"></head></head>
+
+
+<link rel="stylesheet" href="..\Styles\products.css"></head>
+
+<html>
+
+<?php include 'header.php'; ?>
+
+<body>
+	<br/><br/><br/><br/></br></br>
+	
+	<div id="col">
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+		<?php include 'article.php'; ?>
+	
+		
+	</div>
+
+	
+</body>
+</html>
