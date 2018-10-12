6f5c419f15256c38f58044d040caa1efe3c9d231
charlesGdrt <charles.goudaert@isen.yncrea.fr>
2018-10-12 15:36:15 +0200

    recovery

---
+++ Pages/researchedProduct.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<head><link rel="stylesheet" href="..\Styles\main.css">
+<link rel="stylesheet" href="..\Styles\article.css">
+</head>
+
+<html>
+
+<?php include 'header.php'; ?>
+
+<body>
+<p>
+
+	BODY <!--Pas de solution pour l'instant-->
+	<br/><br/><br/><br/><br/>
+	
+	you researched a product -> work in progress
+	<?php include 'article.php'; ?>
+	</p>
+</body>
+</html>
+
+
