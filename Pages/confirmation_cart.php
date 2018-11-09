2bf865b0b2223b014a79acf52f9bfeebddc1efaf
charlesGdrt <charles.goudaert@isen.yncrea.fr>
2018-11-09 14:33:20 +0100

    Revert "Merge branch 'Quentin's-Work' into Dynamique_V2"
    This reverts commit c38a6a28bee9ea6f77751441fd703fcbb3e733fd, reversing
    changes made to 8a45f68b277871c8d01b93cc113e594a1dd84a4c.

--- Pages/confirmation_cart.php
+++ Pages/confirmation_cart.php
@@ -1,12 +1,5 @@
 <!doctype html>
-<<<<<<< HEAD
 <head>
-=======
-<head><link rel="stylesheet" href="..\Styles\main.css">
-<link rel="stylesheet" href="..\Styles\article.css">
-<link rel="stylesheet" href="..\Styles\header.css">
-<link rel="stylesheet" href="..\Styles\conf_cart.css">
->>>>>>> Quentin's-Work
 <meta http-equiv="refresh" content="0; URL=cart.php">
 </head>
 
@@ -15,30 +8,14 @@
 
 
 <body>
-<<<<<<< HEAD
 <?php
-=======
-<?php include 'header.php'; 
->>>>>>> Quentin's-Work
     
 	
 	$id=$_POST['ID'];
 	$quantity = $_POST['Quantity'];
 	$unit_price = $_POST['Unit_price'];
 	
-<<<<<<< HEAD
 include '../Database/database.php'; 
-=======
-	try
-{
-$bdd = new PDO('mysql:host=localhost;dbname=dump', 'root', '');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-	die('Erreur : '.$e->getMessage());
-}  
->>>>>>> Quentin's-Work
     
 	$sql = "INSERT INTO order_products(id, order_id, quantity, unit_price, created_at, updated_at) 
 	VALUES('$id', '1', '$quantity', '$unit_price', '2018-10-11 15:50:23', '2018-10-11 15:50:23')
--- Pages/researchedProduct.php
+++ Pages/researchedProduct.php
@@ -16,7 +16,6 @@
 	<br/><br/><br/><br/><br/>
 	
 <?php
-<<<<<<< HEAD
 include '../Database/database.php';
 
 $research = $_GET['search']; 
@@ -28,62 +27,6 @@ include 'affichage_product.php';
 
 ?>
 		
-=======
-try
-{
-$bdd = new PDO('mysql:host=localhost;dbname=dump', 'root', '');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-	die('Erreur : '.$e->getMessage());
-}
-
-$research = $_GET['search']; 
-$researchMAJ = strtoupper($research);
-$request="SELECT name, UPPER(name), image, unit_price, id, description
-		FROM products";
-$reponse = $bdd->query($request);
-
-while ($donnees = $reponse->fetch())
-{
-if ($donnees['UPPER(name)'] == $researchMAJ){
-	?><TABLE align="center">
-	<TH>
-	<?php
-	echo "<img src='".$donnees['image']."'/>";
-		?>
-		</TH>
-		</br>
-		<TD>
-		<div class="name">
-		<?php
-		echo $donnees['name'];?>
-		</div>
-		<div class="description">
-		<?php echo $donnees['description'];?>
-		</div>
-		</br>
-		<?php
-		echo $donnees['unit_price'];
-		?>
-		€
-		
-		<form method="POST" action="confirmation_cart.php" id="quantity">
-	<label></label>
-	<input type="int" name="Quantity" size="1" maxlength ="3" value="">
-	<input type="hidden" name = "ID" value = "<?php echo $donnees['id'] ?>">
-	<input type="hidden" name = "Unit_price" value = "<?php echo $donnees['unit_price'] ?>">
-	<input type="submit" value ="Add to cart" name = "Add to cart">
-	</form></TD>
-<?php
-}
-}
-$reponse->closeCursor();
-
-?>
-	
->>>>>>> Quentin's-Work
 </p>
 </body>
 </html>
