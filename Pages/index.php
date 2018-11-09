86a8f69629423ae16a3d858df180a6da4abfa1c1
Quentin <quentin.consigny@isen.yncrea.fr>
2018-11-09 13:45:31 +0100

    Modif minim
    Un changement dans la bdd pour tester une partie du css + log de la bdd faite dans l'index

--- Pages/index.php
+++ Pages/index.php
@@ -1,9 +1,36 @@
 <!doctype html>
+
+<?php
+
+
+//TODO start session
+
+//TODO include database.php file
+	$user ='root';
+	$pass = '';
+	$db = 'dump';
+	
+	$mysqli = new mysqli('localhost', $user, $pass, $db) or die ("Unable to connect");
+	if($mysqli->connect_error){
+		die('Error');
+	}
+//TODO include checkUser.php file
+
+//TODO get page parameter ($_GET['page'] or $_POST['page']) and assign it into $page variable
+
+//if 'action/'.$page'.php' exists then include it (use file_exists($filename) function)
+		//create one php file for each action to manage on the website
+
+		//TODO use 
+		//             input params (included in $_GET or $_POST)
+		//             $database variable (initialized in $database.php) 
+		// to insert or update data into database
+
+?>
+
 <head>
 <link rel="stylesheet" href="..\Styles\main.css">
-
 <link rel="stylesheet" href="..\Styles\article.css">
-
 <link rel="stylesheet" href="..\Styles\header.css">
 </head>
 
