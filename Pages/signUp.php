ebac46f160061e77a6a6ee44d03e82c44a1847e9
relous1 <43474927+relous1@users.noreply.github.com>
2018-11-23 17:07:41 +0100

    com
    fini de faire le signup et les infomation complementaire.
    et fini la connextion session

--- Pages/signUp.php
+++ Pages/signUp.php
@@ -10,6 +10,22 @@
 		<br/>
     <label for="psw"><em>Password</em></label><br/>
     <input type="password" id="psw" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+    <br/>
+    <label for="usrname"><em>nom personne</em></label><br/>
+    <input type="text" id="human_name" name="human_name" required>
+    <br/>
+    <label for="usrname"><em>Adresse de livraison</em></label><br/>
+    <input type="text" id="address_one" name="address_one" required>
+    <br/>
+    <label for="usrname"><em>Adresse de facturation</em></label><br/>
+    <input type="text" id="postal_code" name="postal_code" required>
+    <br/>
+    <label for="usrname"><em>Ville</em></label><br/>
+    <input type="text" id="city" name="city" required>
+    <br/>
+    <label for="usrname"><em>Pays</em></label><br/>
+    <input type="text" id="country" name="country" required>
+    <br/>
 		<br/>
     <input type="submit" value="Submit">
 	<input type="hidden" name="page" value="main">
